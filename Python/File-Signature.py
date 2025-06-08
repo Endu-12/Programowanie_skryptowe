fff264e2b00a788646e6b6bbb04477e7d919ec2f
Endu-12 <283796@student.pwr.edu.pl>
2025-06-08 19:22:33 +0200

    Add files via upload

--- Python/File-Signature.py
+++ Python/File-Signature.py
@@ -2,46 +2,61 @@ from Crypto.PublicKey import RSA
 from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
 
+#Function used to generate RSA keys
 def generate_keys():
     key = RSA.generate(2048)
     private_key = key
     public_key = key.publickey()
     return private_key, public_key
 
+#Function used for saving keys
 def save_keys(private_key, public_key):
     with open('private_key.pem', 'wb') as file:
         file.write(private_key.exportKey())
     with open('public_key.pem', 'wb') as file:
         file.write(public_key.exportKey())
 
+#Functions used for signing te file
 def sign(path, private_key):
+    #Reading the file
     with open(path, 'rb') as file:
         data = file.read()
 
+    #Creating hash file and signing it with private key
     hash = SHA256.new(data)
     signature = pkcs1_15.new(private_key).sign(hash)
 
+    #Saving signature to file with .sig extension
     with open(path + ".sig", 'wb') as sig_file:
         sig_file.write(signature)
     print('Signature saved to ' + path + ".sig")
 
+#Function used for verifying the signature
 def verify(path, signature_path, public_key):
+    #Reading file and signature for that file
     with open(path, 'rb') as file:
         data = file.read()
     with open(signature_path, 'rb') as sig_file:
         signature = sig_file.read()
 
+    #Creating hash file
     hash = SHA256.new(data)
+    #try-except for error detection
     try:
+        #Veryfying signature with public key
         pkcs1_15.new(public_key).verify(hash, signature)
         print('Signature verified')
     except (ValueError, TypeError):
         print('Signature not verified')
 
+#Main loop
 if __name__ == '__main__':
+    #Creating public and private keys
     private_key, public_key = generate_keys()
+    #Saving keys
     save_keys(private_key, public_key)
     while True:
+        #Conditional instruction responsible for using option chosen by user
         choice = input('Do you want to sign, verify signature or exit? (s/v/x): ')
         if choice == 's':
             file = input('Enter file path: ')
